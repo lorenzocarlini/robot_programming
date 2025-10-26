0747b17a60df849d031bc7e57679378a02c6b82f
Lorenzo <carlini.lorenzo@protonmail.com>
2025-10-26 12:37:16 +0100

    Optional part of rp_02_cpp_intro's assignments (btree), inclusion of .gitignore

--- exercises/rp_02_cpp_intro/src/btree/btree.cpp
+++ exercises/rp_02_cpp_intro/src/btree/btree.cpp
@@ -18,12 +18,9 @@ TreeNodeInt::~TreeNodeInt(){
 TreeNodeInt* TreeNodeInt::find(int value_) {
   if (value==value_)
     return this;
-  if (value_ < value) {
+  if (value_<value) {
+    // TODO: fill here
     // if there is a left child, continue the search in the left, otherwise return null
-    if (! left) 
-      return 0;
-    return left->find(value_);
-       
   }
   if (value_>value) {
     // if there is a right child, continue the search in the right, otherwise return null
@@ -44,14 +41,9 @@ bool TreeNodeInt::add(int value_) {
     }
     return left->add(value_);
   }
-  if (value_ > value) {
-    if (!right) {
-      right = new TreeNodeInt(value_);
-      return true;
+  if (value_>value) {
+    // TODO: fill here
   }
-  return false;
-  }
-  // Just to shut the IDE up
   return false;
 }
 
